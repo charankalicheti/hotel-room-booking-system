22ce806473d27cee2987f54f6ad60f54539e86d5
charankalicheti <kalicheti.charan123@gmail.com>
2026-06-30 11:40:43 +0530

    changes in main

--- Backend/app/models/room.py
+++ Backend/app/models/room.py
@@ -13,4 +13,4 @@ class Room(Base):
     capacity = Column(Integer, nullable=False)
     description = Column(String(255), nullable=True)
     is_available = Column(Boolean, default=True)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())3
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
